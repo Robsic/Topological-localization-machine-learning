6cbaecb1ecc7d396b8147d7efab5f7a296c4083a
Mateus Silva <mateusfilipi22@unifei.edu.br>
2026-09-19 18:27:53 -0300

    chore: clean generated files from repository

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
-from gui.interface import run_gui
+from src.gui.interface import run_gui
 
 if __name__ == "__main__":
     run_gui()
